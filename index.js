5b8da75b8b11377079e4d2fdb9bf2637734bed61
Andrew Ngo <50420651+WerdnaOGN@users.noreply.github.com>
2024-01-09 12:59:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-const myVariable = process.env.REACT_APP_SECRETDBURL;
-console.log(myVariable)
+//const myVariable = process.env.REACT_APP_SECRETDBURL;
+//console.log(myVariable)
 
 const appSettings = {
     databaseURL: "https://realtime-database-42c5a-default-rtdb.firebaseio.com/"
